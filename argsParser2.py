8cfad7ab49d5732b4405a1ffb04c6c365ad1e9ef
Marco Bomben <marco.bomben@lpnhe.in2p3.fr>
2015-05-23 15:43:31 +0200

    Add argsParser2.py

---
+++ argsParser2.py
@@ -0,0 +1,17 @@
+import argparse
+
+def parser2(arguments): 
+
+  parser = argparse.ArgumentParser(description='Prepare deck file.')
+  parser.add_argument('--template',nargs=1, type=str, help='name of the template deck file',required=True)
+  #parser.add_argument('--entry_point','-i', nargs=2, metavar=('x', 'y'), help='track entry point',required=True)
+  #parser.add_argument('--exit_point','-o', nargs=2, metavar=('x', 'y'), help='track exit point',required=True)
+  parser.add_argument('--pitch','-p', nargs=1, type=float, help='pixel pitch [um]',required=True)
+  parser.add_argument('--thickness','-w', nargs=1, type=float, help='pixel thickness [um]',required=True)
+  parser.add_argument('--theta','-t', nargs=1, type=float, help='track impinging angle with respect to the surface [deg]',required=True)
+  parser.add_argument('--deltay','-y', nargs=1, type=float, help='trackin resolution [um]',required=True)
+  parser.add_argument('--t0','-0', nargs=1, type=float, help='start time [s]',required=True)
+
+
+  args = parser.parse_args(arguments)
+  return args
