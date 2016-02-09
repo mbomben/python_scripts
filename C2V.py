97e6d255ca055bbc3190dc5f76efece05333afb2
Marco Bomben <marco.bomben@gmail.com>
2016-02-09 22:19:13 +0100

    adding C2V.py; it saves a C2V version of the CV file

---
+++ C2V.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+def C2V(filename):
+
+
+  input_file = open(filename,'r')
+
+  input_lines = input_file.readlines()
+
+  basename=os.path.splitext(filename)[0]
+  ext = os.path.splitext(filename)[1]
+  savefile = "%s_C2V%s" % (basename,ext)
+  print "savefile is",savefile
+
+  output_file = open(savefile,'w')
+
+  nlines = len(input_lines)
+
+
+  for line in input_lines:
+    tmp = line.split()
+    X = float(tmp[0])
+    Y = float(tmp[1])
+    newY = 1./Y/Y
+    output_file.write('%f %e\n' % (X,newY))
+
+if __name__ == "__main__":
+  if (len(sys.argv)!=2):
+    print "Usage:",sys.argv[0],"<filename>"
+    exit(2)
+  filename = sys.argv[1]
+  C2V(filename)
