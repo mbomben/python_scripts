d98d3e5e1e26b74fca9455b125e4e7cdcdba1513
Marco Bomben <marco.bomben@gmail.com>
2016-03-02 15:31:27 +0100

    Adding griddata.py potential_razor.py; potential.dat. Run potential_razor.py to get a contour plot map of the potential.dat data

---
+++ potential_razor.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import griddata
+
+X = []
+Y = []
+V = []
+for line in open('potential.dat'):
+  tmp = line.split()
+  x = float(tmp[0])
+  y = float(tmp[1])
+  v = float(tmp[2])
+  X.append(x)
+  Y.append(y)
+  V.append(v)
+
+npr = np.random
+npts = len(X)                           # the total number of data points.
+X = np.asarray(X)            # x data in array format
+Y = np.asarray(Y)            # y data in array format
+V = np.asarray(V)            # v data in array format
+
+# plot some profiles / cross-sections for some visualization.  our
+# function is a symmetric, upward opening paraboloid z = x**2 + y**2.
+# We expect it to be symmetric about and and y, attain a minimum on
+# the origin and display minor Gaussian noise.
+
+plt.ion()   # pyplot interactive mode on
+
+# x vs z cross-section.  notice the noise.
+plt.plot(x, v, '.')
+plt.title('X vs V=F(X,Y=constant)')
+plt.xlabel('X')
+plt.ylabel('V')
+
+# y vs z cross-section.  notice the noise.
+plt.plot(y, v, '.')
+plt.title('Y vs V=F(Y,X=constant)')
+plt.xlabel('Y')
+plt.ylabel('V')
+
+# now show the dependent data (x vs y).  we could represent the v data
+# as a third axis by either a 3d plot or contour plot, but we need to
+# grid it first....
+plt.plot(x, y, '.')
+plt.title('X vs Y')
+plt.xlabel('X')
+plt.ylabel('Y')
+
+# enter the gridding.  imagine drawing a symmetrical grid over the
+# plot above.  the binsize is the width and height of one of the grid
+# cells, or bins in units of x and y.
+binsize = 5
+grid, bins, binloc = griddata.griddata(X, Y, V, binsize=binsize)  # see this routine's docstring
+
+
+# minimum values for colorbar. filter our nans which are in the grid
+vmin    = grid[np.where(np.isnan(grid) == False)].min()
+vmax    = grid[np.where(np.isnan(grid) == False)].max()
+
+# colorbar stuff
+palette = plt.matplotlib.colors.LinearSegmentedColormap('jet3',plt.cm.datad['jet'],2048)
+palette.set_under(alpha=0.0)
+
+# plot the results.  first plot is x, y vs v, where v is a filled level plot.
+extent = (X.min(), X.max(), Y.min(), Y.max()) # extent of the plot
+#plt.subplot(1, 2, 1)
+plt.imshow(grid, extent=extent, cmap=palette, origin='lower', vmin=vmin, vmax=vmax, aspect='auto', interpolation='bicubic')
+plt.xlabel(r'$X\, [\mu m]$')
+plt.ylabel(r'$Y\, [\mu m]$')
+plt.title('Potential map')
+cbar = plt.colorbar()
+cbar.set_label(r'$V_{bias}\, [V]$')
+plt.gca().invert_yaxis()
+plt.clim(-500,0)
+plt.savefig('razor.png')
+
+# now show the number of points in each bin.  since the independent data are
+# Gaussian distributed, we expect a 2D Gaussian.
+#plt.subplot(1, 2, 2)
+#plt.imshow(bins, extent=extent, cmap=palette, origin='lower', vmin=0, vmax=bins.max(), aspect='auto', interpolation='bilinear')
+#plt.xlabel('X values')
+#plt.ylabel('Y values')
+#plt.title('X, Y vs The No. of Pts Per Bin')
+#plt.colorbar()
+#plt.show()
+#plt.savefig('razor.png')
