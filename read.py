f502891c4ee2bfaff6b6b3447109381d4d416ee2
Marco Bomben <marco.bomben@gmail.com>
2017-06-02 17:37:11 +0200

    read.py

---
+++ read.py
@@ -0,0 +1,26 @@
+
+import sys
+
+def read(filename):
+  f = open(filename)
+  lines = f.readlines()
+  X=[]
+  Y=[]
+  for line in lines:
+    tmp = line.split()
+    x = float(tmp[0])
+    y = float(tmp[1])
+    X.append(x)
+    Y.append(y)
+  return (X,Y)
+
+if (__name__ == "__main__"):
+  if (len(sys.argv) != 2 ):
+    print "Usage:",sys.argv[0],"<filename>\n";
+    exit(2)
+  filename = sys.argv[1]
+  X=[]
+  Y=[]
+  (X,Y) = read(filename)
+  
+
