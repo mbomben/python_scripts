fe4df4c55144464bd943eac4cd5dbd216d9cb4ea
Marco Bomben <marco.bomben@cern.ch>
2026-01-20 14:27:22 +0100

    remove_tail.py: remove the last n lines from a file, saving a copy with a _trimmed in the filename

---
+++ remove_tail.py
@@ -0,0 +1,33 @@
+import os
+import sys
+if (len(sys.argv) != 3):
+  print("Usage: %s <filename> <number of lines to remove>" % (sys.argv[0]))
+  #print("Usage: %s <filename>\n" % (sys.argv[0]))
+  exit(2)
+filename=sys.argv[1]
+num_of_lines_to_skip = int(sys.argv[2])
+#print os.path.splitext(filename)[0]
+basename=os.path.splitext(filename)[0]
+#print os.path.splitext(filename)[1]
+ext = os.path.splitext(filename)[1]
+
+newname = basename + "_trimmed" + ext
+print("The trimmed file is:\n\t %s" %(newname))
+f = open(filename,'r')
+g = open(newname,'w')
+lines = f.readlines()
+nlines = len(lines)
+n_lines_to_be_kept = nlines - num_of_lines_to_skip
+index = -1
+for line in lines:
+  index = index +1
+  if ( index >= n_lines_to_be_kept ):
+    continue
+  else:
+    tmp = line.split()
+    X = float(tmp[0])  
+    Y = float(tmp[1]) 
+    values = str(X) + ' ' +str(Y) + '\n'
+    g.write(values)
+f.close()
+g.close() 
